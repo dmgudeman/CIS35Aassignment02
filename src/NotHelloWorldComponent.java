f80cec4d7d871078fe4116a08d3ef2d1633a109f
David Gudeman <davidmgudeman@gmail.com>
2015-07-21 08:42:18 -0700

    working

--- src/NotHelloWorldComponent.java
+++ src/NotHelloWorldComponent.java
@@ -32,18 +32,25 @@ public class NotHelloWorldComponent extends JComponent
     public void paintComponent(Graphics g)
     {
         ArrayList<Double[]> pointsLeft = new ArrayList<>(this.list);
-        Graphics2D g2d = (Graphics2D) g.create();
+
         System.out.println("hoooooooooooooooooooooooooooPoints Left         ooo" + list.size());
-        //AffineTransform at = AffineTransform.getTranslateInstance(30, 50);
+
+      //  AffineTransform transformer  = new AffineTransform();
+       //         transformer.translate(-35, +107);
+        //        transformer.scale(100, 100 );
+        Graphics2D g2d = (Graphics2D) g;
+     //   g2d.setTransform(transformer);
+
         ArrayList<Boolean> viableIndices = new ArrayList<>();
 
 
-        double x1, x2, y1, y2;
+      //  double x1, x2, y1, y2;
         double distance = 0;
         double xBase, yBase, xIdent, yIdent;
         int closestIndex = 0;
         int baseIndex = 18;
         list.get(0)[3] = 1.0;
+
 
 
         for (int j = this.list.size() - 1; j > 0; j--)
@@ -55,27 +62,29 @@ public class NotHelloWorldComponent extends JComponent
                 System.out.println("CLosest INDEX   " + closestIndex);
                 this.list.get(closestIndex)[3] = 1.0;
 
-                int ix1 = ((int)((this.list.get(baseIndex)[0]-35)*100));
+                int ix1 = (((int)((this.list.get(baseIndex)[0]-34.5)*100))+0);
+                int x1 = ((int)((this.list.get(baseIndex)[0])*1));
                 System.out.println("this.list.get(baseIndex[0] = " + this.list.get(baseIndex)[0]);
                 System.out.println("this.list.get(baseIndex[0]-35 = " + (this.list.get(baseIndex)[0]-35));
                 System.out.println("(this.list.get(baseIndex[0]-35)*100) = " + ((this.list.get(baseIndex)[0]-35)*100));
                 System.out.println("x1 (int) (this.list.get(baseIndex[0]-35)*100) = " + ((int)((this.list.get(baseIndex)[0]-35)*100)));
 
                 System.out.println("x2 (int) (this.list.get(closestIndex[0]-35)*100) = " + ((int)((this.list.get(closestIndex)[0]-35)*100)));
-                System.out.println("y1 (int) (this.list.get(baseIndex[1]-104)*100) = " + ((int)((-1*(this.list.get(baseIndex)[1])-104)*100)));
-                System.out.println("y2 (int) (this.list.get(closestIndex[1]-104)*100) = " + ((int)((-1*(this.list.get(closestIndex)[1])-104)*100)));
-                int ix2 = ((int)((this.list.get(closestIndex)[0]-35)*100));
+                System.out.println("y1 (int) (this.list.get(baseIndex[1]-104)*100) = " + ((int)(((this.list.get(baseIndex)[1])+107)*100)));
+                System.out.println("y2 (int) (this.list.get(closestIndex[1]-104)*100) = " + ((int)(((this.list.get(closestIndex)[1])+107)*100)));
+                int ix2 = (((int)((this.list.get(closestIndex)[0]-34.5)*100)));
 
-                int  iy1 = (int) (((-1*(this.list.get(baseIndex)[1]))-104)*100);
-                int iy2 = (int) ((-1*(this.list.get(closestIndex)[1])-104) *100);
+                int  iy1 = (int) ((((this.list.get(baseIndex)[1]))+107)*250);
+                int iy2 = (int) (((this.list.get(closestIndex)[1])+107) *250);
            //     g2d.transform(at);
                 System.out.println("Base Index = " + baseIndex + " ix1 = " + ix1 + " x2 = " + iy1);
                 System.out.println("Closest Index = " + closestIndex + " ix2 = " + ix2 + " iy2 = " + iy2);
                 System.out.println("____________________________________________");
                 System.out.println("\n");
-                g2d.drawLine(ix1, iy1, ix2, iy2);
 
-
+            //
+              // g2d.drawLine((int)((this.list.get(baseIndex)[0])*1), (int)((this.list.get(baseIndex)[1])*1), (int)((this.list.get(closestIndex)[0])*1), (int)((this.list.get(closestIndex)[1])*1));
+                 g2d.drawLine(ix1, iy1, ix2, iy2);
             }
             baseIndex = closestIndex;
         }
